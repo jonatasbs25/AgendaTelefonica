b4e608ac060251238d63e1cd47cde6d95cecaad7
Jônatas Borges de Souza <jonatasbsouza@hotmail.com>
2022-03-02 23:40:26 -0300

    Conexão com o banco

---
+++ AgendaTelefonica.API/Data/Banco.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AgendaTelefonica.API.Data
+{
+    //implementação da interface IDisposable
+    //permite que o Garbage Colletor libere objetos da memória após seu uso
+    public class Banco : IDisposable
+    {
+        //váriavel de leitura que gerenciará a conexão com o banco
+        private readonly IDbConnection connection;
+
+        //ao iniciar o objeto Banco, receberá como parâmetro as informações
+        //contidas no arquivo de configuração da aplicação e abrirá a conexão
+        public Banco(IConfiguration configuration)
+        {
+            //a váriavel abre a conexão pegando as informações do banco que estão
+            //no arquivo appsettings.json
+            connection = new SqlConnection(configuration
+                .GetConnectionString("DefaultConnection"));
+            connection.Open();
+        }
+
+        //o método Dispose() fechará a conexão após seu uso e liberará
+        //o espaço alocado na memória 
+        public void Dispose() 
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+        } 
+    }
+}
--- AgendaTelefonica.API/Startup.cs
+++ AgendaTelefonica.API/Startup.cs
@@ -1,3 +1,4 @@
+using AgendaTelefonica.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -32,6 +33,10 @@ namespace AgendaTelefonica.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "AgendaTelefonica.API", Version = "v1" });
             });
+
+            //registra o serviço do banco e permite o compartilhamento
+            //da mesma instância (dados) do objeto dentro de uma chamada
+            services.AddScoped<Banco>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
