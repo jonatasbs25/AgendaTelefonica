4f1704e3786b2676c502884a25906502b5293624
Jônatas Borges de Souza <jonatasbsouza@hotmail.com>
2022-03-02 21:36:17 -0300

    Criação do modelo

---
+++ AgendaTelefonica.API/Data/Agenda.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaTelefonica.API.Data
+{
+    public class Agenda
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NumContato { get; set; }
+    }
+}
